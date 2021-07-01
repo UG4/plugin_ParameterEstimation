89ba104a162c38b1661c73f8b6e93c75c414a7ef
Tim Schön <timschoen95@googlemail.com>
2021-07-01 16:03:43 +0200

    Added new files to package

--- UGParameterEstimator/evaluators/__init__.py
+++ UGParameterEstimator/evaluators/__init__.py
@@ -1,3 +1,6 @@
 from .evaluator import *
 from .clusterEvaluator import *
 from .localEvaluator import *
+from .schedulingAdapter import *
+from .ugsubmitSlurmSchedulingAdapter import *
+from .ugsubmitQStatSchedulingAdapter import *
